957b0bb1ee12ec644e807ab6ffad10533e99983d
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-24 15:52:31 +0800

    Create MainMenuState.hx

---
+++ source/MainMenuState.hx
@@ -0,0 +1,18 @@
+package;
+
+import flixel.FlxState;
+
+using StringTools;
+
+class MainMenuState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float) 
+	{
+		super.update(elapsed);
+	}
+}
