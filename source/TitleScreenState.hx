e5f563cb94b2b189f0ee089524744bff461d434e
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-24 15:52:26 +0800

    Create TitleScreenState.hx

---
+++ source/TitleScreenState.hx
@@ -0,0 +1,18 @@
+package;
+
+import flixel.FlxState;
+
+using StringTools;
+
+class TitleScreenState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
