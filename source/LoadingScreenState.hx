51163f95f4c51d237fc9448b881fff0faae3d02f
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-24 16:44:34 +0800

    Create LoadingScreenState.hx

---
+++ source/LoadingScreenState.hx
@@ -0,0 +1,18 @@
+package;
+
+import flixel.FlxState;
+
+using StringTools;
+
+class LoadingScreenState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
